c392cd9b56fc4fec1106351ffb12274b5193a3e6
vinor228 <75385389+vinor228@users.noreply.github.com>
2023-01-27 13:23:33 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <div
+        class="relative-position cursor_normal none_outline"
+        style="color: rgb(255, 255, 255); opacity: 1; border-radius: 6px; transform: rotate(0deg); border: 0px solid rgb(70, 144, 247); padding: 10px; text-align: center; letter-spacing: 0px; font-size: 32px; font-family: Aleo; font-weight: 300; text-decoration: none; box-shadow: rgb(0, 0, 0) 0px 0px 0px 0px; text-transform: initial; font-style: normal; z-index: 2002; text-shadow: rgba(0, 0, 0, 0.2) 2px 2px 0px; line-height: 1.5em;"
+        spellcheck="false"
+      >
+        <span style="background-color: rgb(248, 187, 208); padding-left:3px; padding-right: 3px; border-radius: 4px;">
+          Hi
+        </span>
+        <br />
+        <span style="background-color: rgb(248, 187, 208); padding-left:3px; padding-right: 3px; border-radius: 4px;">
+          My lovely
+        </span>
+        <span style="color: rgb(136, 14, 79); font-size: 54px">little</span>
+        <span style="color: rgb(186, 104, 200); background-color: rgb(248, 187, 0);">
+          Ponny
+        </span>
+      </div>
+    </div>
+  );
+}
