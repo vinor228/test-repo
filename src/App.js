d3e28588c6f818f743ca8e524681ee748833b23a
Eduard mortred <eduardmortred@gmail.com>
2023-01-27 13:43:07 +0000

    Update 2 files

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ export default function App() {
         style="color: rgb(255, 255, 255); opacity: 1; border-radius: 6px; transform: rotate(0deg); border: 0px solid rgb(70, 144, 247); padding: 10px; text-align: center; letter-spacing: 0px; font-size: 32px; font-family: Aleo; font-weight: 300; text-decoration: none; box-shadow: rgb(0, 0, 0) 0px 0px 0px 0px; text-transform: initial; font-style: normal; z-index: 2002; text-shadow: rgba(0, 0, 0, 0.2) 2px 2px 0px; line-height: 1.5em;"
         spellcheck="false"
       >
+        npm install --save react-draft-wysiwyg draft-js
         <span style="background-color: rgb(248, 187, 208); padding-left:3px; padding-right: 3px; border-radius: 4px;">
           Hi
         </span>
